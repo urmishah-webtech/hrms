f440874a9bb21c6e04ea6769e10eef552fe14544
mark122 <mithilchauhan@gmail.com>
2021-12-04 15:06:19 +0530

    indicator model

---
+++ app/Indicator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Indicator extends Model
+{
+    public function designation()
+    {
+        return $this->belongsTo('App\Designation');
+    }
+}
