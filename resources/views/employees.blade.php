f6c95f4f5dc6525dc45435a1a8ab83d66323c8de
devangivadariya <devangivadariya1996@gmail.com>
2022-01-03 19:41:37 +0530

    New EMployee

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -166,7 +166,7 @@
 												<option value="">Select Role</option>
 												@isset($roles)
                                                     @foreach ($roles as $item)
-                                                    <option value="{{ $item->name }}">{{ $item->name }}</option> 
+                                                    <option value="{{ $item->id }}">{{ $item->name }}</option> 
                                                     @endforeach
                                                 @endisset
                                             </select>
