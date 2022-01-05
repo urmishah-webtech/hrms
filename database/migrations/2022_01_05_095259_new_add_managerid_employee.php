95370edf6a4427cdfb176d57540ca264fdc8acea
devangivadariya <devangivadariya1996@gmail.com>
2022-01-05 16:05:20 +0530

    remance manager

--- database/migrations/2022_01_05_095259_new_add_managerid_employee.php
+++ database/migrations/2022_01_05_095259_new_add_managerid_employee.php
@@ -14,7 +14,7 @@ class NewAddManageridEmployee extends Migration
     public function up()
     {
         Schema::table('employees', function (Blueprint $table) {            
-			$table->bigInteger('manager_id')->nullable()->unsigned();
+			$table->bigInteger('man_id')->nullable()->unsigned();
         });
     }
 
