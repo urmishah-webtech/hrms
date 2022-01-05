7025638c20f91100498e223609e210e420950ca9
devangivadariya <devangivadariya1996@gmail.com>
2022-01-05 12:55:46 +0530

    second warning table

--- app/EmployeeSecondVerbalWarning.php
+++ app/EmployeeSecondVerbalWarning.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeSecondVerbalWarning extends Model
 {
     //
-<<<<<<< HEAD
+ 
 }
-=======
-} 
->>>>>>> aef672221f17b480870a1296a080359457ed3b51
+ 
