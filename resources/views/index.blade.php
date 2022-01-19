b747b0ef329337461b409f3014442d3f65fdf0f9
devangivadariya <devangivadariya1996@gmail.com>
2022-01-19 19:43:07 +0530

    Index Module

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -66,6 +66,146 @@
         </div>
 
         <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title">Total Revenue</h3>
+                                <div id="bar-charts"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title">Sales Overview</h3>
+                                <div id="line-charts"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+					 
+        <!-- Statistics Widget -->
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-xl-4 d-flex">
+                <div class="card flex-fill dash-statistics">
+                    <div class="card-body">
+                        <h5 class="card-title">Statistics</h5>
+                        <div class="stats-list">
+                            <div class="stats-info">
+                                <p>Today Leave <strong>{{$on_leave}} <small>/ {{$total_emp}}</small></strong></p>
+                                <div class="progress">
+                                    <div class="progress-bar @if($progress_leave <= 30) bg-primary @elseif($progress_leave >= 31 || $progress_leave <= 60)   bg-warning @else bg-danger @endif" role="progressbar" style="width: {{$progress_leave}}%" aria-valuenow="{{$progress_leave}}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="stats-info">
+                                <p>Planned Leaves <strong>{{$plan_count}} <small>/ {{$on_leave}}</small></strong></p>
+                                <div class="progress">
+                                    <div class="progress-bar @if($plan_data <= 30) bg-primary @elseif($plan_data >= 31 || $plan_data <= 60)   bg-warning @else bg-danger @endif" role="progressbar" style="width: {{$plan_data}}%" aria-valuenow="{{$plan_data}}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="stats-info">
+                                <p>Unplanned Leaves <strong>{{$unplan_data}} <small>/ {{$on_leave}}</small></strong></p>
+                                <div class="progress">
+                                    <div class="progress-bar @if($unplan_count <= 30) bg-primary @elseif($unplan_count >= 31 || $unplan_count <= 60)   bg-warning @else bg-danger @endif" role="progressbar" style="width: {{$unplan_count}}%" aria-valuenow="{{$unplan_count}}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="stats-info">
+                                <p>Pending Requests <strong>{{$pending_req}}</strong></p>
+                                <div class="progress">
+                                    <div class="progress-bar @if($pending_persent <= 30) bg-primary @elseif($pending_persent >= 31 || $pending_persent <= 60)   bg-warning @else bg-danger @endif" role="progressbar" style="width: {{$pending_persent}}%" aria-valuenow="{{$pending_persent}}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <!--<div class="stats-info">
+                                <p>Closed Tickets <strong>22 <small>/ 212</small></strong></p>
+                                <div class="progress">
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 22%" aria-valuenow="22" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>--->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-12 col-lg-6 col-xl-4 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-body">
+                        <h4 class="card-title">Task Statistics</h4>
+                        <div class="statistics">
+                            <div class="row">
+                                <div class="col-md-6 col-6 text-center">
+                                    <div class="stats-box mb-4">
+                                        <p>Total Tasks</p>
+                                        <h3>385</h3>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-6 text-center">
+                                    <div class="stats-box mb-4">
+                                        <p>Overdue Tasks</p>
+                                        <h3>19</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-purple" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">30%</div>
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: 22%" aria-valuenow="18" aria-valuemin="0" aria-valuemax="100">22%</div>
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 24%" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100">24%</div>
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: 26%" aria-valuenow="14" aria-valuemin="0" aria-valuemax="100">21%</div>
+                            <div class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="14" aria-valuemin="0" aria-valuemax="100">10%</div>
+                        </div>
+                        <div>
+                            <p><i class="fa fa-dot-circle-o text-purple mr-2"></i>Completed Tasks <span class="float-right">166</span></p>
+                            <p><i class="fa fa-dot-circle-o text-warning mr-2"></i>Inprogress Tasks <span class="float-right">115</span></p>
+                            <p><i class="fa fa-dot-circle-o text-success mr-2"></i>On Hold Tasks <span class="float-right">31</span></p>
+                            <p><i class="fa fa-dot-circle-o text-danger mr-2"></i>Pending Tasks <span class="float-right">47</span></p>
+                            <p class="mb-0"><i class="fa fa-dot-circle-o text-info mr-2"></i>Review Tasks <span class="float-right">5</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-12 col-lg-6 col-xl-4 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-body">
+                        <h4 class="card-title">Today Absent <span class="badge bg-inverse-danger ml-2">{{$on_leave}}</span></h4>
+                        @isset($on_leave_data)
+                        @foreach($on_leave_data as $val)
+                        <div class="leave-info-box">
+                            <div class="media align-items-center">
+                                <a href="profile" class="avatar"><img alt="" src="img/user.jpg"></a>
+                                <div class="media-body">
+                                    <div class="text-sm my-0">{{$val->employee->first_name}}</div>
+                                </div>
+                            </div>
+                            <div class="row align-items-center mt-3">
+                                <div class="col-6">
+                                    <h6 class="mb-0">{{date('d M Y', strtotime(@$val->from_date))}} - {{date('d M Y', strtotime(@$val->to_date))}}</h6>
+                                    <span class="text-sm text-muted">Leave Date</span>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <span class="badge bg-inverse-danger">
+                                    @if(@$val->status==1)Pending	
+                                    @elseif(@$val->status==2)Approved
+                                    @else Disapproved @endif</span>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        @endisset
+                        <div class="load-more text-center">
+                            <a class="text-dark" href="{{ url('leaves') }}">Load More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Statistics Widget -->
+
+        <div class="row">
             <div class="col-md-6 d-flex">
                 <div class="card card-table flex-fill">
                     <div class="card-header">
