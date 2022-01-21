cf14d7c937f7c0ec6dffdb6a7cb0d753c372e832
Dikshitajain <dikshitajaindj20@gmail.com>
2022-01-21 15:33:23 +0530

    promotion stat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,10 +74,11 @@ class HomeController extends Controller
         $last_month_emp_count=Employee::whereMonth('created_at', '=', Carbon::now()->subMonth()->month)->where('role_id','!=',1)->get()->count();
         $current_month_emp_count=Employee::whereMonth('created_at', '=', Carbon::now()->month)->where('role_id','!=',1)->get()->count();
         $emp_per=(($current_month_emp_count-$last_month_emp_count)/$current_month_emp_count)*100;
-       
+        $promotion_month = Promotion::whereMonth('date', '=', Carbon::now()->month)->get();
+        $promotion_previousmonth = Promotion::whereMonth('date', '=', Carbon::now()->subMonth()->month)->get();
         $currentyear = Carbon::now()->year;
         $lastsixyears = [$currentyear];
-        for ($i=1; $i < 7; $i++) { 
+        for ($i=1; $i < 7; $i++) {
             array_push($lastsixyears, $currentyear-$i);
         }
         $newemp = [];
@@ -97,7 +98,7 @@ class HomeController extends Controller
             $final[$key] = $data;
         }
         $linechartdata = json_encode($final);
-        return view('index',compact('emp_total','per_status_complete','per_status_incomp','man_total', 'emp', 'res', 'promotion', 'appraisal','on_leave','on_leave_data','total_emp','progress_leave','plan_count','unplan_count','pending_persent','unplan_data','plan_data','pending_req', 'linechartdata'));
+        return view('index',compact('emp_total','per_status_complete','per_status_incomp','man_total', 'emp', 'res', 'promotion', 'appraisal','on_leave','on_leave_data','total_emp','progress_leave','plan_count','unplan_count','pending_persent','unplan_data','plan_data','pending_req', 'linechartdata', 'promotion_month', 'promotion_previousmonth'));
     }
 
     public function editPromotion(){
@@ -144,7 +145,7 @@ class HomeController extends Controller
         $res = Resignation::orderBy('id', 'DESC')->limit(3)->get();
         $promotion = Promotion::orderBy('id', 'DESC')->limit(5)->get();
         $appraisal = Appraisal::orderBy('id', 'DESC')->limit(5)->get();
-        
+
 		return view('index',compact('emp_total','per_status_complete','per_status_incomp','man_total', 'emp', 'res', 'promotion', 'appraisal','on_leave','on_leave_data','total_emp','progress_leave','plan_count','unplan_count','pending_persent','unplan_data','plan_data','pending_req'));
 		}
 		else
--- resources/views/percentage_stat.blade.php
+++ resources/views/percentage_stat.blade.php
@@ -8,35 +8,35 @@
                             <span class="d-block">New Employees</span>
                         </div>
                         <div>
-                            <span class="text-success">+10%</span>
+                            <span class="text-success"><?php $emp_percent = ($emp_total/100)*100; ?>{{ $emp_percent }}%</span>
                         </div>
                     </div>
-                    <h3 class="mb-3">10</h3>
+                    <h3 class="mb-3">{{ $emp_total }}</h3>
                     <div class="progress mb-2" style="height: 5px;">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{ $emp_percent }}%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <p class="mb-0">Overall Employees {{ $emp_total }}</p>
+                    <p class="mb-0">Overall Employees <span class="text-muted">{{ $emp_total }}</span></p>
                 </div>
             </div>
-        
+
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between mb-3">
                         <div>
-                            <span class="d-block">Earnings</span>
+                            <span class="d-block">Promotions</span>
                         </div>
                         <div>
-                            <span class="text-success">+12.5%</span>
+                            <span class="text-success"><?php $promotion_percent = (count($promotion_month)/100)*100; ?>{{$promotion_percent}}%</span>
                         </div>
                     </div>
-                    <h3 class="mb-3">$1,42,300</h3>
+                    <h3 class="mb-3">{{@count($promotion_month)}}</h3>
                     <div class="progress mb-2" style="height: 5px;">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{$promotion_percent}}%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <p class="mb-0">Previous Month <span class="text-muted">$1,15,852</span></p>
+                    <p class="mb-0">Previous Month <span class="text-muted">{{@count($promotion_previousmonth)}}</span></p>
                 </div>
             </div>
-        
+
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between mb-3">
@@ -54,7 +54,7 @@
                     <p class="mb-0">Previous Month <span class="text-muted">$7,500</span></p>
                 </div>
             </div>
-        
+
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between mb-3">
@@ -73,5 +73,5 @@
                 </div>
             </div>
         </div>
-    </div>	
+    </div>
 </div>
