d0221db40fc594a71e58cf5ba8d340bec1b1ff18
devangivadariya <devangivadariya1996@gmail.com>
2022-01-24 12:36:58 +0530

    manager perfomance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -208,6 +208,22 @@
                                 </div>
                             </div>
                         </div>
+                        @if(Auth::user()->role_id == 2)
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-purple" role="progressbar" style="width: {{round($man_width_80100, 2)}}%" aria-valuenow="{{round($man_width_80100, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($man_width_80100, 2)}}%</div>
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: {{round($man_width_6079, 2)}}%" aria-valuenow="{{round($man_width_6079, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($man_width_6079, 2)}}%</div>
+                            <div class="progress-bar bg-success" role="progressbar" style="width: {{round($man_width_4059, 2)}}%" aria-valuenow="{{round($man_width_4059, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($man_width_4059, 2)}}%</div>
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: {{round($man_width_2039, 2)}}%" aria-valuenow="{{round($man_width_2039, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($man_width_2039, 2)}}%</div>
+                            <div class="progress-bar bg-info" role="progressbar" style="width: {{round($man_width_119, 2)}}%" aria-valuenow="{{round($man_width_119, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($man_width_119, 2)}}%</div>
+                        </div>
+                        <div>
+                            <p><i class="fa fa-dot-circle-o text-purple mr-2"></i>100 - 80 %<span class="float-right">{{$man_excel_80100}}</span></p>
+                            <p><i class="fa fa-dot-circle-o text-warning mr-2"></i>80 - 60 %<span class="float-right">{{$man_excel_6079}}</span></p>
+                            <p><i class="fa fa-dot-circle-o text-success mr-2"></i>60 - 40 %<span class="float-right">{{$man_excel_4059}}</span></p>
+                            <p><i class="fa fa-dot-circle-o text-danger mr-2"></i>40 - 20 %<span class="float-right">{{$man_excel_2039}}</span></p>
+                            <p class="mb-0"><i class="fa fa-dot-circle-o text-info mr-2"></i>20 - 1 %<span class="float-right">{{$man_excel_119}}</span></p>
+                        </div>
+                        @else
                         <div class="progress mb-4">
                             <div class="progress-bar bg-purple" role="progressbar" style="width: {{round($width_80100, 2)}}%" aria-valuenow="{{round($width_80100, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($width_80100, 2)}}%</div>
                             <div class="progress-bar bg-warning" role="progressbar" style="width: {{round($width_6079, 2)}}%" aria-valuenow="{{round($width_6079, 2)}}" aria-valuemin="0" aria-valuemax="100">{{round($width_6079, 2)}}%</div>
@@ -222,6 +238,7 @@
                             <p><i class="fa fa-dot-circle-o text-danger mr-2"></i>40 - 20 %<span class="float-right">{{$excel_2039}}</span></p>
                             <p class="mb-0"><i class="fa fa-dot-circle-o text-info mr-2"></i>20 - 1 %<span class="float-right">{{$excel_119}}</span></p>
                         </div>
+                        @endif
                     </div>
                 </div>
             </div>
