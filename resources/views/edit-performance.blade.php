694cd2381c48ebc6ba76462e7fdeeb251b998325
Urmi Shah <urmi@webtech-evolution.com>
2022-04-06 13:57:55 +0530

    setting module

--- resources/views/edit-performance.blade.php
+++ resources/views/edit-performance.blade.php
@@ -1031,6 +1031,19 @@
             
         </div>
         <!-- /Page Wrapper -->
-        
+    
+	 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+		 <?php  
+		 $hr_id = Employee::where('role_id',5)->pluck('id')->first();
+		$compl= Employee::where('performance_completed_by',$hr_id)->pluck('id')->toArray();   foreach($compl as $val){
+		if($emp_hrcomp == $val){?>
+		jQuery("input").attr('disabled','disabled');
+		jQuery("select").attr('disabled','disabled');
+		jQuery(".btn-add-row").css('pointer-events','none');
+		});
+		<?php }
+		} ?>
+		</script>	 
        @endsection
- 
