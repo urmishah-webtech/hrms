928ac18176fb4379fdf34717c2c4598fb1e696b6
Urmi Shah <urmi@webtech-evolution.com>
2022-04-12 15:05:43 +0530

    terminations error

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -20,6 +20,7 @@
                         </div>
                     </div>
                 </div>
+              
                 <!-- /Page Header -->
                 @if ($errors->any())
                 <div class="alert alert-danger alert-block">
@@ -303,6 +304,9 @@
                         
                 </div>
             </div>
+        @if(!$emp_profile)
+        <h4>Employee is Removed</h4>
+        @else
             @include('change_emp_info')
             <!-- Personal Info Modal -->
             <div id="personal_info_modal" class="modal custom-modal fade" role="dialog">
@@ -702,7 +706,7 @@
                 </div>
             </div>
             <!-- /Experience Modal -->
-            
+            @endif
         </div>
         <!-- /Page Wrapper -->
 @endsection
